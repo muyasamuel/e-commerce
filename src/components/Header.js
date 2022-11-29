a58beddd45ef156c8e6e9474a1bb85d8640d2420
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-11-29 13:34:22 +0300

    Introduce a nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='bg-red-500 h-14 flex  '> E-Commerce </div>
+    <div className='bg-red-500 h-14 flex items-center justify-between px-8  '>
+      <h1>E-Commerce </h1>
+      <nav >
+        <ul className='flex space-x-8 text-white capitalize'>
+          <li>Home</li>
+          <li>Feed</li>
+          <li>About</li>
+          <li>Contact </li>
+        </ul>
+      </nav>
+       
+       </div>
   )
 }
 
