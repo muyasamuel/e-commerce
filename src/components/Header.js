637459f58da33b6162c6cb971550daf3301b4ca8
muyasamuel <muya.samuel@students.jkuat.ac.ke>
2022-12-02 23:45:16 +0300

    Adding contents to sub sections

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
-    <div className='bg-red-500 h-14 flex items-center justify-between px-8  '>
-      <h1>E-Commerce </h1>
-      <nav >
-        <ul className='flex space-x-8 text-white capitalize'>
-          <li>Home</li>
-          <li>Feed</li>
-          <li>About</li>
-          <li>Contact </li>
-        </ul>
-      </nav>
-       <div className='bg-yellow'>yellow</div>
-       </div>
-       
-  )
+    <div>
+      <div className="bg-red-500 h-14 flex items-center justify-between px-8  ">
+        <h1>E-Commerce </h1>
+        <nav>
+          <ul className="flex space-x-12 text-white capitalize">
+            <li>Home</li>
+            <li>Feed</li>
+            <li>About</li>
+            <li>Contact </li>
+          </ul>
+        </nav>
+      </div>
+      <div className="bg-yellow-500 h-9 flex items-center justify-center ">
+      <ul className=" flex space-x-12 text-white capitalize">
+            <li>Latest Products </li>
+            <li>Offers</li>
+            <li>Warrant Products</li>
+            <li>Free Delivery</li>
+          </ul>
+      </div>
+      
+    </div>
+  );
 }
 
-export default Header
+export default Header;
